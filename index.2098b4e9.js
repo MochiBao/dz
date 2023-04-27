1304874523f8a750d6d9b768e0254d4d147aba0c
MochiBao <113111181+MochiBao@users.noreply.github.com>
2023-04-27 14:53:47 +0000

    Deploying to gh-pages from @ MochiBao/dz@55ee61d6ea238d159aa470b2db1559f7eecc0e8b 🚀

---
+++ index.2098b4e9.js
@@ -0,0 +1,2 @@
+!function(){var e="USER";localStorage.setItem(e,JSON.stringify({name:"Lox",age:100}));var a=localStorage.getItem(e),t=JSON.parse(a);t.age=30,localStorage.setItem(e,JSON.stringify(t))}();
+//# sourceMappingURL=index.2098b4e9.js.map
---
+++ index.fa2c0596.js
@@ -0,0 +1,2 @@
+localStorage.setItem("USER",JSON.stringify({name:"Lox",age:100}));const e=localStorage.getItem("USER"),t=JSON.parse(e);t.age=30,localStorage.setItem("USER",JSON.stringify(t));
+//# sourceMappingURL=index.fa2c0596.js.map
