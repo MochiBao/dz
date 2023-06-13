bea6ce313d177737711c593426908973d1c5dc2e
MochiBao <soneshka189@gmail.com>
2023-06-13 17:18:21 +0300

    ря

--- src/js/re.js
+++ src/js/re.js
@@ -1,42 +1,70 @@
-const refs = {
-    textField: document.querySelector(".textareaItem"),
-    counter: document.querySelector(".counter span"),
+// const refs = {
+//     textField: document.querySelector(".textareaItem"),
+//     counter: document.querySelector(".counter span"),
 
-    div: document.querySelector(".div"),
+//     div: document.querySelector(".div"),
+// };
+
+// const limit = textField.getAttribute(".maxlenght");
+// refs.counter.innerHTML = limit;
+
+// refs.textField.addEventListener("keyup", onKeyUp);
+// refs.textField.addEventListener("keydown", onKeyDown);
+
+// function onKeyDown(params) {
+//    if (e.repeat) {
+//      onKeyUp()
+//    };
+// };
+
+// function onKeyUp(e) {
+//     const limitText = limit - textField.value.trim().length;
+//     refs.counter.innerHTML = limitText;
+// };
+
+// refs.div.addEventListener("dblclick", onDblclick);
+// refs.div.addEventListener("contextmenu", onContextMenu);
+
+// function onContextMenu(e) {
+//     e.preventDefault()
+//     console.log(e);
+// };
+
+
+// function onDblclick(e) {
+//     console.log(e);
+// };
+
+// window.addEventListener("scroll", onScroll);
+
+// function onScroll(e) {
+//     console.log(`${Math.round(scrollY)} px`);
+// };
+
+/////
+
+const dev = document.createElement("ul"); //створення чогось в DOM-дереві
+for (let index = 0; index < 5; 0++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = "Привіт";
+    console.log(listItem);
+    dev.appendChild(listItem);
 };
 
-const limit = textField.getAttribute(".maxlenght");
-refs.counter.innerHTML = limit;
+console.log(dev);
+console.log(document);
 
-refs.textField.addEventListener("keyup", onKeyUp);
-refs.textField.addEventListener("keydown", onKeyDown);
+const body = document.body;
 
-function onKeyDown(params) {
-   if (e.repeat) {
-     onKeyUp()
-   };
-};
-
-function onKeyUp(e) {
-    const limitText = limit - textField.value.trim().length;
-    refs.counter.innerHTML = limitText;
-};
-
-refs.div.addEventListener("dblclcik", onDblclick);
-refs.div.addEventListener("contextmenu", onContextMenu);
-
-function onContextMenu(e) {
-    e.preventDefault()
-    console.log(e);
-};
+body.append(dev);
 
 
-function onDblclick(e) {
-    console.log(e);
-};
 
-window.addEventListener("scroll", onScroll);
+const arr = [1, 2, 3, 4, 5];
+const markup = arr.map(number => {
+   return `<li>${number}</li>`
+}).join("");
 
-function onScroll(e) {
-    console.log(`${Math.round(scrollY)} px`);
-}
+dev.innerHTML = markup;
+dev.insertAdjacentElement("beforeend", markup);
+
